bb498265c90676b8b3c6601cdf0d80c8474b1913
sei0o <sei0o@live.jp>
2014-09-20 13:19:59 +0900

    bugfix of bug fix of bug fix

--- lib/clier.rb
+++ lib/clier.rb
@@ -7,18 +7,19 @@ module Clier
 		last_key = :key
 		
 		argv.each do |arg|
+			p arg
 			case arg
 				when /--?(.+)/ # -h, -a, -b, -9, --help, --action, --beta
 					# get the first letter of (.+)  e.g. -h => h, --help => h
 					last_key = Regexp.last_match[1][0].to_sym
 					parsed[last_key] = true
 				when /(.+)/ # value
-					parsed[last_key] = Regexp.last_match[1].to_sym # replace true => value
+					parsed[last_key] = Regexp.last_match[1] # replace true => value
 			end
 		end
 		
-		parsed[:key] = parsed[:key].to_s if parsed[:key] 
-		
+		parsed[:key] = parsed[:key].to_s if parsed[:key]
+	
 		parsed
 	end
 	
